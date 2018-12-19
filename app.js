38247c08dc1514dab4583434bf67ba0ec766e702
afozbek <furkanozbek1995@gmail.com>
2018-12-19 20:55:20 +0300

    building_the_api

---
+++ app.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const MONGO_URI = 'mongodb+srv://afozbek:admin@myprojects-ggr2u.mongodb.net/messages';
+
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(paht.join(__dirname, 'images')));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+})
+
+
+mongoose.connect(MONGO_URI)
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
---
+++ controllers/feed.js
@@ -0,0 +1,51 @@
+const { validationResult } = require('express-validator/check');
+
+const Post = require('../models/post');
+
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{
+
+      _id: '1',
+      title: 'First Post',
+      content: 'This is the first post!',
+      imageUrl: 'images/Furkan2.jpeg',
+      creator: {
+        name: 'Furkan Ozbek'
+      },
+      createdAt: new Date()
+    }]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed!')
+    error.statusCode = 422;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/Furkan2.jpeg',
+    creator: { name: 'Furkan' },
+  });
+  post.save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    })
+
+};
