5e7c05f76164c804c6c0df0325ad9a6490543062
afozbek <furkanozbek1995@gmail.com>
2018-12-20 20:50:12 +0300

    finishing_login

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const { body } = require('express-validator/check');
 const User = require('../models/user');
 const authController = require('../controllers/auth');
 
+
 const router = express.Router();
 
 router.put('/signup', [
